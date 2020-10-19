d55da005264dce4e55e490167897783d180d9e5a
Lucas Morais <lucas.axm@gmail.com>
2020-10-19 20:27:38 -0300

    distort method implemented

--- services/distort_service.rb
+++ services/distort_service.rb
@@ -1,7 +1,9 @@
 require_relative '../logger/logging'
+require 'rmagick'
+require 'fastimage'
 
 class DistortService
-  include Logging
+  include Logging, Magick
 
   # @param [Bilu::Bot] bilu
   def initialize(bilu)
@@ -11,23 +13,55 @@ class DistortService
   def distort(message)
     return unless message.reply_to_message
     m = message.reply_to_message
-    if m.photo
+    if !m.photo.empty?
+      logger.info('photo found.')
       file_id = m.photo[-1].file_id
       file_name = "#{file_id}.jpg"
-    elsif m.sticker
-      return if m.sticker.is_animated
+    elsif !m.sticker.nil?
+      logger.info('sticker found.')
       file_id = m.sticker.file_id
       file_name = "#{file_id}.webp"
-    elsif m.document
-      return unless m.document.mime_type.startswith("image/")
+    elsif !m.document.nil?
+      return unless m.document.mime_type.start_with? 'image/'
+      logger.info('image as document found.')
       file_id = m.document.file_id
       file_name = "#{file_id}.#{m.document.mime_type.split('/')[1]}"
     end
 
+
     file_path = @bilu.get_file(file_id)
+    logger.info("file_path = '#{file_path}'.")
+
+    return if File.extname(file_path) == '.tgs'
+
+    @bilu.bot.api.send_chat_action(
+        chat_id: message.chat.id,
+        action: 'upload_photo'
+    )
     temp_file = @bilu.download_file(file_path, file_name)
 
+    width, height = FastImage.size(temp_file.path)
+    img = ImageList.new(temp_file.path)
 
+    temp_file.close
+
+    img = img.liquid_rescale(width*0.35, height*0.35, 3, 5)
+    img.resize!(width, height)
+    #img = img.negate
+    img.write(file_name)
+    img.destroy!
+
+    upload = Faraday::UploadIO.new(file_name, 'image/jpeg')
+    @bilu.bot.api.send_photo(
+        chat_id: message.chat.id,
+        photo: upload,
+        reply_to_message_id: m.message_id
+    )
+    upload.close
+    logger.info('scaled photo sent.')
+
+
+    FileUtils.rm(file_name)
 
   end
 
