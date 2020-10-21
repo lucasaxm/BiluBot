453469d6c10f86f56aae0b99c89f5a4f454e33a7
Lucas Morais <lucas.axm@gmail.com>
2020-10-21 16:59:07 -0300

    deep fry feature

--- controllers/image_controller.rb
+++ controllers/image_controller.rb
@@ -1,15 +1,19 @@
 require_relative '../logger/logging'
-require_relative '../services/distort_service'
+require_relative '../services/image_service'
 
-class DistortController
+class ImageController
   include Logging
 
   # @param [Bilu::Bot] bilu Telegram bot instance
   def initialize(bilu)
-    @service = DistortService.new(bilu)
+    @service = ImageService.new(bilu)
   end
 
   def distort(message, chat)
     @service.distort(message)
+  end
+
+  def deepfry(message, chat)
+    @service.deepfry(message)
   end
 end
