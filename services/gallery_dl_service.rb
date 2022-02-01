221206684b127274fb70354f974d2e24d84b7979
Lucas <lucas.axm@gmail.com>
2022-01-31 21:17:42 -0300

    reddit_service.rb using gallery_dl_service.rb

--- services/gallery_dl_service.rb
+++ services/gallery_dl_service.rb
@@ -8,12 +8,13 @@ require_relative '../logger/logging'
 class GalleryDLService
   include Logging
 
-  def initialize(bilu, message)
+  def initialize(bilu, message, reddit_post = nil)
     GalleryDLConfig.save_config
     @bilu = bilu
     @message = message
     @dir = './gallerydl'
     @uploads_chat_id = ENV['BILU_UPLOADS_TELEGRAM_ID']
+    @reddit_post = reddit_post
   end
 
   def clean_dir
@@ -27,11 +28,17 @@ class GalleryDLService
       format: 'best[filesize<?20M]/best',
       output: filepath
     }
-    result = Timeout::timeout(300, nil, "YoutubeDL.download timeout. url=[#{@message.text}] options=[#{options}]") do
-      YoutubeDL.download @message.text, options
+    result = Timeout.timeout(300, nil, "YoutubeDL.download timeout. url=[#{@message.text}] options=[#{options}]") do
+      if !@reddit_post.nil? && !@reddit_post.url.nil?
+        YoutubeDL.download @reddit_post.url, options
+      else
+        YoutubeDL.download @message.text, options
+      end
     end
     if result.nil?
       logger.error 'Failed to download video using youtube-dl'
+      raise Telegram::Bot::Exceptions::Base, 'GalleryDL Service error' unless @reddit_post.nil?
+
       return
     end
     result.information[:category] = result.information[:extractor]
@@ -39,20 +46,36 @@ class GalleryDLService
     FileUtils.mv(filepath, new_filepath)
     result.information[:local_path] = new_filepath
     send_gallerydl_media(GalleryDL::Media.new(@message.text, {}, [result.information]))
+  rescue => e
+    raise e if @reddit_post.nil?
+
+    logger.error e.message
+    raise Telegram::Bot::Exceptions::Base, 'GalleryDL Service error'
   ensure
-    logger.warn `pkill -e youtube-dl`
-    logger.warn `rm -fv #{filepath}`
-    logger.warn `rm -rfv #{filepath}`
+    logger.warn "killing youtube-dl #{`pkill -e youtube-dl`}"
+    logger.warn "cleaning files #{filepath} #{`rm -fv #{filepath}`}"
+    logger.warn "cleaning directories #{filepath} #{`rm -rfv #{filepath}`}"
   end
 
   def send_media
     logger.info "Trying to send #{@message.text} as media"
     options = {}
-    result = Timeout::timeout(300, nil, "GalleryDL.download timeout. url=[#{@message.text}] options=[#{options}]") do
-      GalleryDL.download @message.text, options
+    result = Timeout.timeout(300, nil, "GalleryDL.download timeout. url=[#{@message.text}] options=[#{options}]") do
+      if @reddit_post.nil?
+        GalleryDL.download @message.text, options
+      else
+        permalink_result = GalleryDL.download "reddit.com#{@reddit_post.permalink}", options
+        if permalink_result.information.empty? && !@reddit_post.url.nil?
+          GalleryDL.download @reddit_post.url, options
+        else
+          permalink_result
+        end
+      end
     end
     if result.nil? || result.information.nil? || result.information.any? { |r| r[:local_path].nil? }
       logger.error 'Failed to download media using gallery-dl'
+      raise Telegram::Bot::Exceptions::Base, 'GalleryDL Service error' unless @reddit_post.nil?
+
       return
     end
     send_gallerydl_media(result)
@@ -60,13 +83,14 @@ class GalleryDLService
     gallery_dl_errors = e.message.each_line.grep /\[error\]/
     if gallery_dl_errors.empty?
       logger.warn "Failed to send media. message: #{e.message}"
+      raise Telegram::Bot::Exceptions::Base, 'GalleryDL Service error' unless @reddit_post.nil?
     else
       logger.warn "Failed to send media. message: #{gallery_dl_errors}"
       fallback_youtubedl
     end
   ensure
-    logger.warn `pkill -e gallery-dl`
-    logger.warn `rm -rf gallery-dl`
+    logger.warn "killing gallery-dl #{`pkill -e gallery-dl`}"
+    logger.warn "cleaning gallery-dl local dir #{`rm -rf gallery-dl`}"
   end
 
   def build_caption(information)
@@ -81,14 +105,21 @@ class GalleryDLService
       "#{information[:title]}:\n#{information[:description]}"
     when 'mangadex'
       "#{information[:manga]}\nChapter #{information[:chapter]}"
+    when 'reddit'
+      "#{information[:over_18] ? "\u{1F51E} NSFW " : ''}#{information[:spoiler] ? "\u{26A0} SPOILER" : ''}\n#{information[:title]}"
     else
-      information[:category]
+      if @reddit_post.nil?
+        information[:category]
+      else
+        "#{@reddit_post.over_18 ? "\u{1F51E} NSFW " : ''}#{@reddit_post.spoiler ? "\u{26A0} SPOILER" : ''}\n#{@reddit_post.title}"
+      end
     end[0..1023]
   end
 
   def send_gallerydl_media(result)
     logger.info "#{result.information.size} medias found from #{@message.text}"
-    group_caption = build_caption(result.information.first) if result.information.length > 1
+    first_caption = nil
+    messages_sent = []
     result.information.each_slice(10) do |information_group|
       media = information_group.map do |information|
         filepath = information[:local_path]
@@ -96,14 +127,26 @@ class GalleryDLService
           logger.info("adding #{filepath} to media group")
           if @bilu.is_local_image?(filepath)
             caption = build_caption(information)
-            group_caption = nil unless group_caption == caption
-            local_photo_media(filepath, caption)
+            if first_caption.nil?
+              first_caption = caption
+              local_photo_media(filepath, first_caption)
+            elsif first_caption == caption
+              local_photo_media(filepath)
+            else
+              local_photo_media(filepath, caption)
+            end
           else
             file_size_mb = @bilu.file_size_mb(filepath)
             if (File.extname(filepath) == '.mp4') && (file_size_mb < 20)
               caption = build_caption(information)
-              group_caption = nil unless group_caption == caption
-              local_video_media(filepath, caption)
+              if first_caption.nil?
+                first_caption = caption
+                local_video_media(filepath, first_caption)
+              elsif first_caption == caption
+                local_video_media(filepath)
+              else
+                local_video_media(filepath, caption)
+              end
             else
               error_msg = "Error sending file #{filepath}:#{file_size_mb}MB"
               logger.warn(error_msg)
@@ -115,19 +158,21 @@ class GalleryDLService
           FileUtils.rm("#{filepath}.json") if File.exist?("#{filepath}.json")
         end
       end
-      @bilu.bot.api.send_media_group(
+      response = @bilu.bot.api.send_media_group(
         chat_id: @message.chat.id,
         reply_to_message_id: @message.message_id,
         media: media
       )
+      messages_sent.push(*response['result'])
     end
-    return if group_caption.nil?
+    return if @reddit_post.nil?
 
-    @bilu.bot.api.send_message(
-      chat_id: @message.chat.id,
-      reply_to_message_id: @message.message_id,
-      text: group_caption
-    )
+    @bilu.bot.api.send_message({
+                                 chat_id: @message.chat.id,
+                                 reply_to_message_id: messages_sent.first['message_id'],
+                                 text: "#{messages_sent.size} media#{'s' if messages_sent.size > 1} found",
+                                 reply_markup: RedditService.reddit_post_reply_markup(@reddit_post)
+                               })
   end
 
   def upload_to_telegram(type, data)
@@ -149,7 +194,7 @@ class GalleryDLService
     end
   end
 
-  def local_photo_media(filepath, caption)
+  def local_photo_media(filepath, caption = nil)
     file_ext = File.extname(filepath)
     case file_ext
     when '.jpeg', '.jpg'
@@ -162,11 +207,12 @@ class GalleryDLService
       logger.error "file extension #{file_ext} is not valid"
       return
     end
-    {
+    media = {
       type: 'photo',
-      media: upload_to_telegram('photo', upload),
-      caption: caption
+      media: upload_to_telegram('photo', upload)
     }
+    media[:caption] = caption unless caption.nil?
+    media
   end
 
   def send_local_video(file_path, caption)
@@ -189,13 +235,14 @@ class GalleryDLService
 
   private
 
-  def local_video_media(filepath, caption)
+  def local_video_media(filepath, caption = nil)
     upload = Faraday::UploadIO.new(filepath, 'video/mp4')
-    {
+    media = {
       type: 'video',
-      media: upload_to_telegram('video', upload),
-      caption: caption
+      media: upload_to_telegram('video', upload)
     }
+    media[:caption] = caption unless caption.nil?
+    media
   end
 
 end
--- services/reddit_service.rb
+++ services/reddit_service.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'open-uri'
 require 'telegram/bot'
 require 'nokogiri'
@@ -19,17 +21,18 @@ class RedditService
     @reddit_session = RedditConfig.new_reddit_session
     @bilu = bilu
     @message = message
-    @available_posts = 0
     @callback = nil
   end
 
   def get_media_from_subreddit_callback(chat)
-    @callback = @message
+    newtext = @message.data.split(' ')[1..-1].join(' ')
+    @message = @message.message
+    @message.text = newtext
     get_media_from_subreddit(chat)
   end
 
   def is_banned?(subreddit, chat)
-    !(BannedSubreddit.find_by(chat_id: chat.id, subreddit_id: subreddit.id).nil?)
+    !BannedSubreddit.find_by(chat_id: chat.id, subreddit_id: subreddit.id).nil?
   end
 
   def ban_subreddit(chat)
@@ -100,34 +103,58 @@ class RedditService
       return
     end
     subreddit_name = text_array[1]
-    subreddit_db = get_subreddit_from_db(subreddit_name)
-    return if subreddit_db.nil?
+    # Reddit API block
+    begin
+      subreddit_db = get_subreddit_from_db(subreddit_name)
+      return if subreddit_db.nil?
 
-    logger.info("searching for /r/#{subreddit_name}.")
-    error_count ||= 0
-    if !chat.nsfw? && subreddit_db.nsfw?
-      answer = 'NSFW subreddits are banned.'
+      logger.info("searching for /r/#{subreddit_name}.")
+      error_count ||= 0
+      if !chat.nsfw? && subreddit_db.nsfw?
+        answer = 'NSFW subreddits are banned.'
+        @bilu.reply_with_text(answer, @message)
+        return
+      end
+      if is_banned?(subreddit_db, chat)
+        answer = 'This subreddit is banned'
+        @bilu.reply_with_text(answer, @message)
+        return
+      end
+      @bilu.bot.api.send_chat_action(
+        chat_id: get_telegram_chat_id,
+        action: 'typing'
+      )
+      # hot_posts = get_subreddit_hot_media_posts(subreddit_name)
+      hot_posts = get_subreddit_hot_posts(subreddit_name)
+    rescue Redd::NotFound, JSON::ParserError => e
+      answer = "subreddit #{subreddit_name} not found."
+      logger.error(answer)
+      logger.error("Exception Class: [#{e.class.name}]")
+      logger.error("Exception Message: [#{e.message}']")
+      @bilu.reply_with_text(answer, @message)
+      return
+    rescue Redd::InvalidAccess => e
+      error_count += 1
+      @reddit_session.client.refresh
+      if error_count < 5
+        logger.warn('Reddit session refreshed. Retrying.')
+        sleep(1)
+        retry
+      end
+      return
+    rescue Redd::Forbidden => e
+      answer = "Access to this subreddit is forbidden. Reason: #{e.message.split[1]}"
       @bilu.reply_with_text(answer, @message)
       return
     end
-    if is_banned?(subreddit_db, chat)
-      answer = 'This subreddit is banned'
-      @bilu.reply_with_text(answer, @message)
-      return
-    end
-    @bilu.bot.api.send_chat_action(
-      chat_id: get_telegram_chat_id,
-      action: 'typing'
-    )
-    hot_posts = get_subreddit_hot_media_posts(subreddit_name)
     if hot_posts.empty?
       answer = "subreddit #{subreddit_name} has no media to send."
       logger.warn(answer)
       @bilu.reply_with_text(answer, @message)
       return
     end
-    post_to_send = nil
-    hot_posts.each_with_index do |hot_post, i|
+    post_sent = false
+    hot_posts.to_a.each_with_index do |hot_post, i|
       reddit_post = RedditPost.find_or_initialize_by(reddit_id: hot_post.id)
       if reddit_post.new_record?
         reddit_post.assign_attributes(title: hot_post.title,
@@ -144,35 +171,21 @@ class RedditService
         next
       end
       reddit_post.chats << chat
-      reddit_post.save
       logger.info("Sending ##{i + 1} of #{hot_posts.size} posts.")
-      @available_posts = hot_posts.size - (i + 1)
-      post_to_send = hot_post
+      reddit_post.save
+      begin
+        send_media(hot_post)
+      rescue Telegram::Bot::Exceptions::Base => e
+        logger.error("Error sending post, trying next post if available. Exception Message: [#{e.message}]")
+        next
+      end
+      post_sent = true
       break
     end
-    if post_to_send.nil?
-      logger.warn('There are no posts left to send. Sending "no available" message.')
-      answer = 'No available posts in this subreddit right now.'
-      @bilu.reply_with_text(answer, @message)
-    else
-      send_media(post_to_send)
-    end
-  rescue Redd::NotFound, JSON::ParserError => e
-    answer = "subreddit #{subreddit_name} not found."
-    logger.error(answer)
-    logger.error("Exception Class: [#{e.class.name}]")
-    logger.error("Exception Message: [#{e.message}']")
-    @bilu.reply_with_text(answer, @message)
-  rescue Redd::InvalidAccess => e
-    error_count += 1
-    @reddit_session.client.refresh
-    if error_count < 5
-      logger.warn('Reddit session refreshed. Retrying.')
-      sleep(1)
-      retry
-    end
-  rescue Redd::Forbidden => e
-    answer = "Access to this subreddit is forbidden. Reason: #{e.message.split[1]}"
+    return if post_sent
+
+    logger.warn('There are no posts left to send. Sending "no available" message.')
+    answer = 'No available posts in this subreddit right now.'
     @bilu.reply_with_text(answer, @message)
   end
 
@@ -189,6 +202,21 @@ class RedditService
       return
     end
     send_media(post)
+  end
+
+  def self.reddit_post_reply_markup(post)
+    Telegram::Bot::Types::InlineKeyboardMarkup.new(
+      inline_keyboard: [[
+                          Telegram::Bot::Types::InlineKeyboardButton.new(
+                            text: "#{post.num_comments} Comments",
+                            url: "https://www.reddit.com#{post.permalink}"
+                          ),
+                          Telegram::Bot::Types::InlineKeyboardButton.new(
+                            text: "More from r/#{post.subreddit.display_name}",
+                            callback_data: "callback /r #{post.subreddit.display_name}"
+                          )
+                        ]]
+    )
   end
 
   private
@@ -209,7 +237,7 @@ class RedditService
     logger.error("Exception Class: [#{e.class.name}]")
     logger.error("Exception Message: [#{e.message}']")
     @bilu.reply_with_text(answer, @message)
-    return nil
+    nil
   end
 
   def reddit_post_from_id(post_id)
@@ -218,9 +246,10 @@ class RedditService
 
   def send_media(post)
     logger.debug("Post: score=[#{post.score}] title=[#{post.title}] url=[#{post.url}]")
-    return if post.is_self
+    raise Telegram::Bot::Exceptions::Base, 'Self post' if post.is_self
+
     url_extension = post.url.split('.').last
-    if ['gif', 'gifv'].include?(url_extension)
+    if %w[gif gifv].include?(url_extension)
       send_gifv(post)
     elsif url_extension == 'mp4'
       send_mp4(post)
@@ -235,13 +264,19 @@ class RedditService
     elsif (post.instance_variable_get :@attributes)[:is_gallery]
       send_gallery(post)
     else
-      send_photo(post)
+      gallery_dl_service = GalleryDLService.new(@bilu, @message, post)
+      Timeout.timeout(30, Telegram::Bot::Exceptions::Base, 'GalleryDl Service Timeout.') {
+        gallery_dl_service.send_media
+      }
+      # send_photo(post)
     end
   end
 
   def send_local_mp4(post, filepath)
     logger.debug("START - Sending #{filepath} as video through telegram API.")
     new_filepath = filepath
+    # new_filepath = "#{SecureRandom.hex}.mp4"
+    # @bilu.transcode_video_to_mp4(filepath, new_filepath)
     @bilu.bot.api.send_chat_action(
       chat_id: get_telegram_chat_id,
       action: 'upload_video'
@@ -252,23 +287,13 @@ class RedditService
       video: upload,
       caption: reddit_post_caption(post),
       reply_to_message_id: get_telegram_message_id,
-      reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-        inline_keyboard: reddit_post_buttons(post)
-      )
+      reply_markup: RedditService.reddit_post_reply_markup(post)
     )
     upload.close
     FileUtils.rm(filepath) if File.exist?(filepath)
     FileUtils.rm("#{filepath}.json") if File.exist?("#{filepath}.json")
     FileUtils.rm(new_filepath) if File.exist?(new_filepath)
     logger.debug("END - Sending #{filepath} as video through telegram API.")
-  end
-
-  def input_text_message_content(post)
-    "<a href=\"#{make_telegram_html_url(post.is_self ? reddit_post_full_permalink(post) : post.url)}\">#{reddit_post_caption(post)}</a>"
-  end
-
-  def reddit_post_full_permalink(post)
-    "https://www.reddit.com#{post.permalink}"
   end
 
   def make_telegram_html_url(url)
@@ -294,27 +319,9 @@ class RedditService
       photo: post.url.to_s,
       caption: reddit_post_caption(post),
       reply_to_message_id: get_telegram_message_id,
-      reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-        inline_keyboard: reddit_post_buttons(post)
-      )
+      reply_markup: RedditService.reddit_post_reply_markup(post)
     )
     logger.debug("END - Sending #{post.url} as photo through telegram API.")
-  end
-
-  def reddit_post_buttons(post)
-    button_array = [
-      Telegram::Bot::Types::InlineKeyboardButton.new(
-        text: "#{post.num_comments} Comments",
-        url: reddit_post_full_permalink(post)
-      )
-    ]
-    if @available_posts.positive?
-      button_array << Telegram::Bot::Types::InlineKeyboardButton.new(
-        text: "Next post from r/#{post.subreddit.display_name}",
-        callback_data: "callback /r #{post.subreddit.display_name}"
-      )
-    end
-    button_array
   end
 
   def send_mp4(post, new_url = nil)
@@ -329,9 +336,7 @@ class RedditService
       video: mp4url,
       caption: reddit_post_caption(post),
       reply_to_message_id: get_telegram_message_id,
-      reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-        inline_keyboard: reddit_post_buttons(post)
-      )
+      reply_markup: RedditService.reddit_post_reply_markup(post)
     )
     logger.debug("END - Sending #{mp4url} as video through telegram API.")
   end
@@ -342,26 +347,32 @@ class RedditService
       chat_id: get_telegram_chat_id,
       action: 'typing'
     )
+    first_caption = nil
+    messages_sent = []
     post.media_metadata.each_slice(10) do |medias|
-      @bilu.bot.api.send_media_group(
+      response = @bilu.bot.api.send_media_group(
         chat_id: get_telegram_chat_id,
         reply_to_message_id: get_telegram_message_id,
         media: medias.map do |_id, metadata|
           logger.debug("Adding #{metadata[:p].last[:u]} to media group.")
-          {
+          media = {
             type: 'photo',
             media: metadata[:p].last[:u]
           }
+          if first_caption.nil?
+            first_caption = reddit_post_caption(post)
+            media[:caption] = first_caption
+          end
+          media
         end
       )
+      messages_sent.push(*response['result'])
     end
     @bilu.bot.api.send_message(
       chat_id: get_telegram_chat_id,
-      text: reddit_post_caption(post),
-      reply_to_message_id: get_telegram_message_id,
-      reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-        inline_keyboard: reddit_post_buttons(post)
-      )
+      reply_to_message_id: messages_sent.first['message_id'],
+      text: "#{messages_sent.size} media#{'s' if messages_sent.size > 1} found",
+      reply_markup: RedditService.reddit_post_reply_markup(post)
     )
     logger.debug('END - Sending media group through telegram API.')
   end
@@ -377,9 +388,7 @@ class RedditService
       document: post.url,
       caption: reddit_post_caption(post),
       reply_to_message_id: get_telegram_message_id,
-      reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-        inline_keyboard: reddit_post_buttons(post)
-      )
+      reply_markup: RedditService.reddit_post_reply_markup(post)
     )
     logger.debug("END - Sending #{post.url} as document through telegram API.")
   end
@@ -427,9 +436,7 @@ class RedditService
       video: new_url,
       caption: reddit_post_caption(post),
       reply_to_message_id: get_telegram_message_id,
-      reply_markup: Telegram::Bot::Types::InlineKeyboardMarkup.new(
-        inline_keyboard: reddit_post_buttons(post)
-      )
+      reply_markup: RedditService.reddit_post_reply_markup(post)
     )
     logger.debug("END - Sending #{new_url} as video through telegram API.")
   end
@@ -452,7 +459,7 @@ class RedditService
   end
 
   def get_subreddit_hot_posts(subreddit)
-    @reddit_session.subreddit(subreddit).hot
+    @reddit_session.subreddit(subreddit).hot.to_a
   end
 
   def send_help_message
